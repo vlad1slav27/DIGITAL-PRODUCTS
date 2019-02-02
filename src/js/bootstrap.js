a7c08a39ecdf308169c923ff901a91fd35bac8f6
Vlad <kalatun4ik@gmail.com>
2019-02-02 16:43:03 +0200

    commit

---
+++ src/js/bootstrap.js
@@ -0,0 +1,13 @@
+//= util.js
+//= alert.js
+//= button.js
+//= carousel.js
+//= collapse.js
+//= dropdown.js
+//= modal.js
+//= popover.js
+//= scrollspy.js
+//= tab.js
+//= toast.js
+//= tooltip.js
+//= scripts.js
